d34a3585dc780e4f1c81c1b6ec4d04cdf64b2f7c
0amr-ashraf0 <46089737+0amr-ashraf0@users.noreply.github.com>
2018-12-30 13:24:48 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,149 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "-";
+client.on('message', async rokz => {
+
+    if(rokz.content.startsWith(prefix + "تقديم")) {
+
+      let lang = '';
+
+      let time = '';
+
+      let expe = '';
+
+      let fillter = m => m.author.id === rokz.author.id
+
+      await rokz.channel.send("ما لغتك ؟").then(e => {
+
+     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+
+     .then(co => {
+
+       lang = co.first().content;
+
+        co.first().delete();
+
+
+       e.edit(`كم هي المدة ؟
+[${lang}]`)
+
+       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+
+       .then(col => {
+
+         time = col.first().content;
+
+          col.first().delete();
+
+
+            e.edit(`ما هي خبرتك ؟
+[${time}]
+[${lang}]`)
+
+            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+
+            .then(coll => {
+
+              expe = coll.first().content;
+
+               coll.first().delete();
+
+
+               e.edit(`جاري تقديمك...
+[${expe}]
+[${time}]
+[${lang}]`)
+
+              let rokzz = rokz.guild.channels.find("name","تقديم")
+
+              setTimeout(() => {
+
+                e.edit("تم التقديم")
+
+              }, 3000)
+
+              rokzz.send(`
+» اللغة : **${lang}**
+» المدة : **${time}**
+» الخبرة : **${expe}**
+تم التقديم بواسطة: ${rokz.author}
+`).then(rokzzz => {
+
+                  rokzzz.react(":CheckMark:")
+
+                  rokzzz.react(":WrongMark:")
+
+                })
+
+            })
+
+       })
+
+     })
+
+   })
+
+    }
+
+  })
+
+ 
+
+    
+
+    
+client.on('message',async message => {
+
+let mention = message.mentions.members.first();
+
+let Room = client.channels.get('528895721454436352');
+
+if(message.content.startsWith(prefix + "رفض")) {
+
+if(message.guild.id !== '528895721454436352') return;
+
+ if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
+
+
+if(!mention) return message.reply("منشن شخص");
+
+
+
+Room.send(`
+**» العضو :** ${mention}
+[ :x: ] :: لقد تم رفض العضو`);
+
+}
+
+});
+  
+
+    
+client.on('message',async message => {
+
+let mention = message.mentions.members.first();
+
+let Room = client.channels.get('528895721454436352');
+
+if(message.content.startsWith(prefix + "قبول")) {
+
+if(message.guild.id !== '528895721454436352') return;
+
+ if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
+
+
+if(!mention) return message.reply("منشن شخص");
+
+
+
+Room.send(`
+**» العضو :** ${mention}
+[ :white_check_mark: ] :: لقد تم قبول العضو واعطائه رتبة سبورت`);
+
+}
+
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
